55085a1be9c357b8d43c054d0757d94f44bcbc58
polyskalov <a@polyskalov.com>
2024-11-18 16:39:16 +0500

    Fix typeorm config for migrations

--- src/common/config/typeorm.ts
+++ src/common/config/typeorm.ts
@@ -1,18 +1,21 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
-export default registerAs(
-  'typeorm',
-  (): TypeOrmModuleOptions => ({
-    type: 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: +process.env.DATABASE_PORT || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME || 'alarmu',
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    migrations: ['dist/migrations/*{.ts,.js}'],
-    autoLoadEntities: true,
-    synchronize: false,
-  }),
-);
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: +process.env.DATABASE_PORT || 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME || 'alarmu',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  autoLoadEntities: true,
+  synchronize: false,
+};
+
+export default registerAs('typeorm', (): TypeOrmModuleOptions => config);
+
+// noinspection JSUnusedGlobalSymbols
+export const connectionSource = new DataSource(config as DataSourceOptions);
